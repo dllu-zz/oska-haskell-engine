39ba4ee6af7d006b709e1ec81afc3b98ef78dde1
Daniel Lu <dllu@dllu.net>
2013-11-04 11:35:47 -0800

    fixed bug in win condition check; mild refactoring of move generator; new multiprint function to display list of states

--- oska.hs
+++ oska.hs
@@ -74,6 +74,13 @@ conv_12wb_a7e7 _ = ' '
 print_a7e7::[Int] -> IO()
 print_a7e7 state = disp_a7e7 state (round (sqrt (fromIntegral (length state))))
 
+-- displays a list of states in the good format
+multiprint_a7e7::[[Int]] -> IO()
+multiprint_a7e7 (x:[]) = print_a7e7 x
+multiprint_a7e7 (x:xs) = do
+        print_a7e7 x
+        multiprint_a7e7 xs
+
 -- displays a state in the good format, given the width of the board
 disp_a7e7::[Int] -> Int -> IO()
 disp_a7e7 [] n = putStrLn " "
@@ -89,14 +96,25 @@ movegen_a7e7::[Int]->Int->[[Int]]
 movegen_a7e7 state who = moveright ++ eatright ++ movedown ++ eatdown 
         where n = length state
               nn = (round (sqrt (fromIntegral (length state))))
-              moveright = [take x state ++ (0:who:(drop (x+2) state)) | x <- [0..(n-3)], canmoveright x]
+              moveright = [a x ++ b x | x <- [0..(n-3)], canmoveright x]
                 where canmoveright x = x/=((div n 2)-1) && (state!!x) == who && (state!!(x+1)) == 0
-              movedown = [take x state ++ 0:(take (nn-1) (drop (x+1) state)) ++ who:(drop (x+nn+1) state) | x <- [0..(n-nn-2)], canmovedown x]
+                      a x = take x state -- we swap the x and x+1 position
+                      b x = 0:who:(drop (x+2) state)
+              movedown = [a x ++ b x ++ c x | x <- [0..(n-nn-2)], canmovedown x]
                 where canmovedown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == 0
-              eatright = [take x state ++ (0:0:who:(drop (x+3) state)) | x<- [0..(n-4)], caneatright x]
+                      a x = take x state -- we swap the x and x+n position
+                      b x = 0:(take (nn-1) (drop (x+1) state))
+                      c x = who:(drop (x+nn+1) state)
+              eatright = [a x ++ b x | x<- [0..(n-4)], caneatright x]
                 where caneatright x = x/=((div n 2)-1) && (state!!x) == who && (state!!(x+1)) == (3-who) && (state!!(x+2)) == 0
-              eatdown = [take x state ++ 0:(take (nn-1) (drop (x+1) state)) ++ 0:(take (nn-1) (drop (x+nn+1) state)) ++ who:(drop (x+2*nn+1) state) | x <- [0..(n-2*nn-2)], caneatdown x]
+                      a x = take x state -- we swap the x and x+2 position and set position x+1 to 0
+                      b x = 0:0:who:(drop (x+3) state)
+              eatdown = [a x ++ b x ++ c x ++ d x | x <- [0..(n-2*nn-2)], caneatdown x]
                 where caneatdown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == (3-who) && (state!!(x+2*nn)) == 0
+                      a x = take x state -- we swap the x and x+2*nn position and set position x+nn to 0
+                      b x = 0:(take (nn-1) (drop (x+1) state))
+                      c x = 0:(take (nn-1) (drop (x+nn+1) state))
+                      d x = who:(drop (x+2*nn+1) state)
 
 -- Eval
 -- Given a board in the good format and which colour of the pieces to move,
@@ -114,8 +132,8 @@ win_a7e7 state who = friendlypieces/=0 && (enemypieces == 0 || friendlypieces ==
               enemypieces = length (filter (==(3-who)) state)
               friendlypieces = length (filter (==who) state)
               backrank 
-                | who == 1 = length (filter (==who) [state!!(i + nn*(2*nn-5-i)) | i <- [(div nn 2)-1..nn-1]])
-                | who == 2 = length (filter (==who) [(reverse state)!!(i + nn*(3*(div nn 4)-2-i)) | i <- [(div nn 2)-1..nn-1]])
+                | who == 1 = length (filter (==who) [state!!(i + nn*(3*(div nn 2)-2-i)) | i <- [(div nn 2)-1..nn-1]])
+                | who == 2 = length (filter (==who) [(reverse state)!!(i + nn*(3*(div nn 2)-2-i)) | i <- [(div nn 2)-1..nn-1]])
 
 -- Engine
 -- Given a state in the good format, which player to move, and the desired ply depth, invokes minimax engine
