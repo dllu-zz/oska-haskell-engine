2bd5de192fa2fa576e16612ff7af7c10f7130471
Daniel Lu <dllu@dllu.net>
2013-11-03 19:29:57 -0800

    implemented functions to convert the given format of an oska board to the integer array to be used internally

--- oska.hs
+++ oska.hs
@@ -1,7 +1,49 @@
-oska::[String] -> Char -> Int
-oska state who depth
-    | valid state = engine (weirdformat2goodformat state) who depth
-    | otherwise = error "Invalid state"
 
-valid::[String] -> Bool
-valid state = True -- todo
+--Daniel Lu 75592063 a7e7
+--Brian Chau 30006118 b8z7
+
+--Project 1 CPSC 312
+
+-- ["wwww","---","--","---","bbbb"]
+
+
+oska_a7e7::[String] -> Char -> Int -> [String]
+oska_a7e7 state who depth
+        | valid_a7e7 state      = []-- goodformat2badformat_a7e7 (engine_a7e7 (badformat2goodformat_a7e7 state) who depth)
+        | otherwise             = error "Invalid state"
+
+-- a valid state must be a list of strings of valid lengths
+-- furthermore, each string must only be made of characters '-', 'w', and 'b'
+valid_a7e7::[String] -> Bool
+valid_a7e7 state = checklengths state && checkcontents state
+        where checklengths x = odd (length x) && (length x) >=4 && checkeachlength x
+                where checkeachlength x = sum [abs (length (x!!i) - (expectedstringlength i))|i<-[0..(length x-1)]] == 0
+                        where expectedstringlength i = 2 + abs (i - (div (length x) 2))
+              checkcontents x = sum [abs (length i - countchar i 'w' - countchar i 'b' - countchar i '-')|i<-x] == 0
+                        where countchar s c = length (filter (==c) s)
+
+badformat2goodformat_a7e7::[String] -> [Int]
+badformat2goodformat_a7e7 state = [getpiece x y | y <-[0..(n-1)], x <-[0..(n-1)]]
+        where n = (length (state!!0))*2 - 2
+              getpiece x y = if okay_a7e7 x y n then conv_wb12_a7e7 (state!!(getx x y)!!(gety x y)) else -1
+                where getx x y = x+y-(div n 2)
+                      gety x y = max (x - (div n 2) + 1) ((div n 2) - y)
+
+okay_a7e7::Int -> Int -> Int -> Bool
+okay_a7e7 x y n = triangles && squares
+        where triangles = (x+y) >= (div n 2) && (x+y) <= (2*n - 2 - (div n 2))
+              squares = not (x<=a && y>=b) && not (x>=b && y<=a)
+                where a = (div n 2) - 2
+                      b = (div n 2) + 1
+
+conv_wb12_a7e7::Char -> Int
+conv_wb12_a7e7 'w' = 1
+conv_wb12_a7e7 'b' = 2
+conv_wb12_a7e7 '-' = 0
+
+conv_12wb_a7e7::Int -> Char
+conv_12wb_a7e7 1 = 'w'
+conv_12wb_a7e7 2 = 'b'
+conv_12wb_a7e7 0 = '-'
+
+
