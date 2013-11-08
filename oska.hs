14ffd252f88e9cefc79beede689c801fb49c59a1
Daniel Lu <dllu@dllu.net>
2013-11-07 16:56:55 -0800

    implemented alpha beta search; new heuristic based on mobility; new heuristic based on popcount; some refactoring; functions to print the bad format states

--- oska.hs
+++ oska.hs
@@ -5,6 +5,8 @@
 --Project 1 CPSC 312
 
 -- ["wwww","---","--","---","bbbb"]
+-- ["bbbb","---","--","---","wwww"]
+-- ["wwwww","----","---","--","---","----","bbbbb"]
 -- ["wwwwww","-----","----","---","--","---","----","-----","bbbbbb"]
 -- ["ww--","--w","-w","-b-","b-bb"]
 -- ["-------","------","-----","----","---","--","---","----","-w---","-b----","b-bbbbb"]
@@ -13,20 +15,33 @@ module Main where
 
 main = do
   let start = ["wwwww","----","---","--","---","----","bbbbb"]
-  let d = 4;
+  let d = 7;
   testing start d
 
 testing :: [String] -> Int -> IO()
 testing start d = do
-  let a = oska_a7e7 start 'w' d
-  print_a7e7 (badformat2goodformat_a7e7 a)
-  let b = oska_a7e7 a 'b' d
-  print_a7e7 (badformat2goodformat_a7e7 b)
+  --let a = oska_a7e7 start 'w' (d-3)
+  let a = oska_ab_a7e7 start 'w' d
+  putStr "W  "
+  --print (minimax_a7e7 (badformat2goodformat_a7e7 start) 1 (d-3))
+  print (alphabeta_a7e7 (badformat2goodformat_a7e7 start) 1 (-3000000) 3000000 d)
+  print_b_a7e7 a
+  --let b = oska_ab_a7e7 a 'b' d
+  let b = oska_a7e7 a 'b' (d-3)
+  putStr "B  "
+  print (minimax_a7e7 (badformat2goodformat_a7e7 a) 2 (d-3))
+  --print (alphabeta_a7e7 (badformat2goodformat_a7e7 a) 2 (-3000000) 3000000 d)
+  print_b_a7e7 b
   if (not (win_a7e7 (badformat2goodformat_a7e7 b) 1)) && (not (win_a7e7 (badformat2goodformat_a7e7 b) 2)) then testing b d else putStrLn "done"
 
 oska_a7e7::[String] -> Char -> Int -> [String]
 oska_a7e7 state who depth
         | valid_a7e7 state      = goodformat2badformat_a7e7 (engine_wrapper_a7e7 (badformat2goodformat_a7e7 state) (conv_wb12_a7e7 who) depth)
+        | otherwise             = error "Invalid state"
+
+oska_ab_a7e7::[String] -> Char -> Int -> [String]
+oska_ab_a7e7 state who depth
+        | valid_a7e7 state      = goodformat2badformat_a7e7 (engine_wrapper_ab_a7e7 (badformat2goodformat_a7e7 state) (conv_wb12_a7e7 who) depth)
         | otherwise             = error "Invalid state"
 
 -- a valid state must be a list of strings of valid lengths
@@ -105,48 +120,79 @@ disp_a7e7 state n = do
         putStrLn [(conv_12wb_a7e7 (state!!i)) | i <- [0..(n-1)]]
         disp_a7e7 (drop n state) n
 
+-- displays a state in the bad format
+print_b_a7e7::[String] -> IO()
+print_b_a7e7 state = disp_b_a7e7 state (length state)
+
+-- displays a state in the bad format, given the length of the board
+disp_b_a7e7::[String] -> Int -> IO()
+disp_b_a7e7 [] n = putStrLn " "
+disp_b_a7e7 (x:xs) n = do
+        putStr [' ' | i <- [1..n - (length x)]]
+        putStrLn (concat [c:' ':[] | c <- x])
+        disp_b_a7e7 xs n
+
+-- Move generator wrapper
+-- If there are no legal moves, outputs original position
+movegen_a7e7::[Int]->Int->[[Int]]
+movegen_a7e7 state who = if not (null newmoves) then newmoves else [state]
+        where newmoves = newmoves_a7e7 state who
+
 -- Move generator
 -- Given a board in the good format and which colour of pieces to move,
 -- returns a list of boards that are reachable in one ply by the specified player
 -- assumes board is rotated such that all pieces only move right and down
--- THERE IS A SERIOUS BUG!!!
--- sometimes it will generate the invalid move of taking a back rank piece and going back to the start.
-movegen_a7e7::[Int]->Int->[[Int]]
-movegen_a7e7 state who = if not (null newmoves) then newmoves else [state]
-        where newmoves = eatright ++ eatdown  ++ moveright ++ movedown
-                where n = length state
-                      nn = (round (sqrt (fromIntegral (length state))))
-                      moveright = [a x ++ b x | x <- [0..(n-3)], canmoveright x]
-                        where canmoveright x = x/=((div n 2)-1) && (state!!x) == who && (state!!(x+1)) == 0
-                              a x = take x state -- we swap the x and x+1 position
-                              b x = 0:who:(drop (x+2) state)
-                      movedown = [a x ++ b x ++ c x | x <- [0..(n-nn-2)], canmovedown x]
-                        where canmovedown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == 0
-                              a x = take x state -- we swap the x and x+n position
-                              b x = 0:(take (nn-1) (drop (x+1) state))
-                              c x = who:(drop (x+nn+1) state)
-                      eatright = [a x ++ b x | x<- [0..(n-4)], caneatright x]
-                        where caneatright x = x/=((div n 2)-1) && (state!!x) == who && (state!!(x+1)) == (3-who) && (state!!(x+2)) == 0
-                              a x = take x state -- we swap the x and x+2 position and set position x+1 to 0
-                              b x = 0:0:who:(drop (x+3) state)
-                      eatdown = [a x ++ b x ++ c x ++ d x | x <- [0..(n-2*nn-2)], caneatdown x]
-                        where caneatdown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == (3-who) && (state!!(x+2*nn)) == 0
-                              a x = take x state -- we swap the x and x+2*nn position and set position x+nn to 0
-                              b x = 0:(take (nn-1) (drop (x+1) state))
-                              c x = 0:(take (nn-1) (drop (x+nn+1) state))
-                              d x = who:(drop (x+2*nn+1) state)
+newmoves_a7e7::[Int]->Int->[[Int]]
+newmoves_a7e7 state who = eatright ++ eatdown  ++ moveright ++ movedown
+        where n = length state
+              nn = (round (sqrt (fromIntegral (length state))))
+              moveright = [a x ++ b x | x <- [0..(n-3)], canmoveright x]
+                where canmoveright x = x/=((div n 2)-1) && (state!!x) == who && (state!!(x+1)) == 0
+                      a x = take x state -- we swap the x and x+1 position
+                      b x = 0:who:(drop (x+2) state)
+              movedown = [a x ++ b x ++ c x | x <- [0..(n-nn-2)], canmovedown x]
+                where canmovedown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == 0
+                      a x = take x state -- we swap the x and x+n position
+                      b x = 0:(take (nn-1) (drop (x+1) state))
+                      c x = who:(drop (x+nn+1) state)
+              eatright = [a x ++ b x | x<- [0..(n-4)], caneatright x]
+                where caneatright x = x/=((div n 2)-1) && ((mod (x+2) nn) == 2 + (mod x nn)) && (state!!x) == who && (state!!(x+1)) == (3-who) && (state!!(x+2)) == 0
+                      a x = take x state -- we swap the x and x+2 position and set position x+1 to 0
+                      b x = 0:0:who:(drop (x+3) state)
+              eatdown = [a x ++ b x ++ c x ++ d x | x <- [0..(n-2*nn-2)], caneatdown x]
+                where caneatdown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == (3-who) && (state!!(x+2*nn)) == 0
+                      a x = take x state -- we swap the x and x+2*nn position and set position x+nn to 0
+                      b x = 0:(take (nn-1) (drop (x+1) state))
+                      c x = 0:(take (nn-1) (drop (x+nn+1) state))
+                      d x = who:(drop (x+2*nn+1) state)
+
+mobility_a7e7::[Int]->Int->Int
+mobility_a7e7 state who = length (eatright ++ eatdown  ++ moveright ++ movedown)
+        where n = length state
+              nn = (round (sqrt (fromIntegral (length state))))
+              moveright = [1 | x <- [0..(n-3)], canmoveright x]
+                where canmoveright x = x/=((div n 2)-1) && (state!!x) == who && (state!!(x+1)) == 0
+              movedown = [1 | x <- [0..(n-nn-2)], canmovedown x]
+                where canmovedown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == 0
+              eatright = [1 | x<- [0..(n-4)], caneatright x]
+                where caneatright x = x/=((div n 2)-1) && ((mod (x+2) nn) == 2 + (mod x nn)) && (state!!x) == who && (state!!(x+1)) == (3-who) && (state!!(x+2)) == 0
+              eatdown = [1 | x <- [0..(n-2*nn-2)], caneatdown x]
+                where caneatdown x = x/=(n-1-(div nn 2)) && (state!!x) == who && (state!!(x+nn)) == (3-who) && (state!!(x+2*nn)) == 0
 
 -- Eval
 -- Given a board in the good format and which colour of the pieces to move,
 -- returns an integer that is the heuristic value of the board
 eval_a7e7::[Int]->Int
 eval_a7e7 state
+  | (win_a7e7 state 1) && (win_a7e7 state 2) = 0
   | win_a7e7 state 1 = 2000000
   | win_a7e7 state 2 = -2000000
-  | otherwise = advanceness state 1 - advanceness (reverse state) 2
+  | otherwise = advanceness state 1 - advanceness (reverse state) 2 + mobility 1 - mobility 2 - popcnt 1 + popcnt 2
         where n = length state
               nn = (round (sqrt (fromIntegral n)))
               advanceness etat who = sum [x+y-(div nn 2) | x<-[0..(nn-1)], y<-[0..(nn-1)], etat!!(x+nn*y)==who]
+              mobility who = mobility_a7e7 state who
+              popcnt who = 3*(length (filter (==who) state))
 
 -- Check win condition
 win_a7e7::[Int]->Int->Bool
@@ -160,22 +206,53 @@ win_a7e7 state who = friendlypieces/=0 && (enemypieces == 0 || friendlypieces ==
                 | who == 2 = length (filter (==who) [(reverse state)!!(i + nn*(3*(div nn 2)-2-i)) | i <- [(div nn 2)-1..nn-1]])
 
 engine_wrapper_a7e7::[Int]->Int->Int->[Int]
-engine_wrapper_a7e7 state 1 depth = snd (max_a7e7 [(engine_a7e7 x 2 depth, x) | x <- movegen_a7e7 state 1])
-engine_wrapper_a7e7 state 2 depth = snd (min_a7e7 [(engine_a7e7 x 1 depth, x) | x <- map reverse (movegen_a7e7 (reverse state) 2)])
+engine_wrapper_a7e7 state 1 depth = snd (max_a7e7 [(minimax_a7e7 x 2 depth, x) | x <- movegen_a7e7 state 1])
+engine_wrapper_a7e7 state 2 depth = snd (min_a7e7 [(minimax_a7e7 x 1 depth, x) | x <- map reverse (movegen_a7e7 (reverse state) 2)])
 
--- Engine
+engine_wrapper_ab_a7e7::[Int]->Int->Int->[Int]
+engine_wrapper_ab_a7e7 state 1 depth = snd (max_a7e7 [(alphabeta_a7e7 x 2 (-3000000) 3000000 depth, x) | x <- movegen_a7e7 state 1])
+engine_wrapper_ab_a7e7 state 2 depth = snd (min_a7e7 [(alphabeta_a7e7 x 1 (-3000000) 3000000 depth, x) | x <- map reverse (movegen_a7e7 (reverse state) 2)])
+
+-- Minimax engine
 -- Given a state in the good format, which player to move, and the desired ply depth, invokes minimax engine
 -- returns heuristic value of best move
-engine_a7e7::[Int]->Int->Int->Int
-engine_a7e7 state who 0 = eval_a7e7 state
-engine_a7e7 state 1 depth 
+minimax_a7e7::[Int]->Int->Int->Int
+minimax_a7e7 state who 0 = eval_a7e7 state
+minimax_a7e7 state who depth 
+  | (win_a7e7 state 1) && (win_a7e7 state 2) = 0
   | win_a7e7 state 1 = 2000000
   | win_a7e7 state 2 = -2000000
-  | otherwise = max_a7e7 [engine_a7e7 x 2 (depth-1) | x <- movegen_a7e7 state 1]
-engine_a7e7 state 2 depth 
+  | otherwise = maxormin [minimax_a7e7 x (3-who) (depth-1) | x <- newmoves]
+          where maxormin
+                  | who == 1 = max_a7e7
+                  | who == 2 = min_a7e7
+                newmoves
+                  | who == 1 = movegen_a7e7 state 1
+                  | who == 2 = map reverse (movegen_a7e7 (reverse state) 2)
+
+-- Alpha-beta engine
+-- Given a state in the good format, which player to move, and the desired ply depth, invokes alpha-beta engine
+-- returns heuristic value of best move
+alphabeta_a7e7::[Int]->Int->Int->Int->Int->Int
+alphabeta_a7e7 state who a b 0 = eval_a7e7 state
+alphabeta_a7e7 state who a b depth
+  | (win_a7e7 state 1) && (win_a7e7 state 2) = 0
   | win_a7e7 state 1 = 2000000
   | win_a7e7 state 2 = -2000000
-  | otherwise = min_a7e7 [engine_a7e7 x 1 (depth-1) | x <- map reverse (movegen_a7e7 (reverse state) 2)]
+  | otherwise = alphabeta_helper_a7e7 newmoves who a b depth
+          where newmoves
+                  | who == 1 = movegen_a7e7 state 1
+                  | who == 2 = map reverse (movegen_a7e7 (reverse state) 2)
+-- helper function for alpha-beta engine
+alphabeta_helper_a7e7::[[Int]]->Int->Int->Int->Int->Int
+alphabeta_helper_a7e7 [] 1 a b depth = a
+alphabeta_helper_a7e7 [] 2 a b depth = b
+alphabeta_helper_a7e7 (x:xs) 1 a b depth = if b <= aa then aa else (alphabeta_helper_a7e7 xs 1 aa b depth)
+          where aa = max a search
+                search = (alphabeta_a7e7 x 2 a b (depth-1))
+alphabeta_helper_a7e7 (x:xs) 2 a b depth = if bb <= a then bb else (alphabeta_helper_a7e7 xs 2 a bb depth)
+          where bb = min b search
+                search = (alphabeta_a7e7 x 1 a b (depth-1))
 
 -- finds maximum of a list
 max_a7e7::Ord a=>[a]->a
